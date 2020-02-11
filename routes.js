cef3be8368b14f29c221a8fc0a1051fc3b418994
Neel Redkar <neel.redkar@outlook.com>
2020-02-10 17:25:47 -0800

    add query function

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 var axios = require("axios")
 var qs = require("qs")
+var cheerio = require("cheerio")
 var reg = /\=(.*?)\;/
 
 const getToken = (user, pass) => {
@@ -15,3 +16,39 @@ const getToken = (user, pass) => {
         })
     })
 }
+const query = (search) => {
+    return new Promise((solve,rej) => {
+        axios.get("https://www.tabroom.com/index/search.mhtml?search="+search)
+            .then((d) => {
+                res = {};
+                var query = cheerio.load(d.data)
+                query("tbody").each((_, table) => {
+                    query(table).children().map((_,row) => {
+                        var name = "";
+                        query(row).children().map((idx,desc) => {
+                            if (idx == 1) {
+                                name = desc.children[1].children[0].data.trim()
+                                res[name] = {}
+                            } else if (idx != 0){
+                                var col = desc.children[0].data.trim().replace(/\n/g,'').replace(/\t/g,' ');
+                                switch (idx) {
+                                    case 2:
+                                        res[name].circuit = col
+                                    case 3:
+                                        res[name].region = col
+                                    case 4:
+                                        res[name].data = col
+                                }
+                            }
+                        })
+                    })
+                })
+                solve(res)
+            })
+            })
+}
+query("stanford")
+    .then((d) => {
+        console.log(d)
+    })
+    
