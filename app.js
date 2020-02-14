9880a13a03b6277037b200d6ab977a50827ed124
Neel Redkar <neel.redkar@outlook.com>
2020-02-13 19:24:26 -0800

    add actual api

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require("express")
+var tools = require("./routes")
+var app = express();
+app.use(express.json())
+
+app.get("/", (req,res) => {
+    res.send("Hi!")
+})
+
+app.post("/api/query", (req,res) => {
+    tools.query(req.body.query)
+        .then((d) => {
+            res.send(d)
+        })
+})
+app.post("/api/tournament/tabs", (req,res) => {
+    tools.getTabs(req.body.tourn_id)
+        .then((d) => {
+            res.send(d)
+        })
+})
+app.post("/api/tournament/events", (req,res) => {
+    tools.fetchEvents(req.body.tourn_id)
+        .then((d) => {
+            res.send(d)
+        })
+})
+app.post("/api/tournament/pairings", (req,res) => {
+    tools.getPairings(req.body.tourn_id,req.body.round_id)
+        .then((d) => {
+            res.send(d)
+        })
+})
+app.post("/api/tournament/rounds", (req,res) => {
+    tools.getRounds(req.body.tourn_id,req.body.event_id)
+        .then((d) => {
+            res.send(d)
+        })
+})
+app.post("/api/tournament/paradigm", (req,res) => {
+    tools.getParadigm(req.body.tourn_id,req.body.judge_id)
+        .then((d) => {
+            res.send(d)
+        })
+})
+
+app.listen(3000, () => console.log("Listening on port 3000!"))
--- routes.js
+++ routes.js
@@ -3,32 +3,31 @@ var qs = require("qs")
 var cheerio = require("cheerio")
 var url = require('url');
 var reg = /\=(.*?)\;/
-
 const getToken = (user, pass) => {
-    return new Promise((res,rej) => {
-        axios({ 
-            method:"POST",
-            url:"https://www.tabroom.com/user/login/login_save.mhtml", 
-            data:qs.stringify({ "maxRedirects": 0, "username": user, "password": pass}),
-            maxRedirects:0
-        })   
-        .catch((data) => {
-            res({token:reg.exec(data.response.headers["set-cookie"][4])[1]})
+    return new Promise((res, rej) => {
+        axios({
+            method: "POST",
+            url: "https://www.tabroom.com/user/login/login_save.mhtml",
+            data: qs.stringify({ "maxRedirects": 0, "username": user, "password": pass }),
+            maxRedirects: 0
         })
+            .catch((data) => {
+                res({ token: reg.exec(data.response.headers["set-cookie"][4])[1] })
+            })
     })
 }
 const query = (search) => {
-    return new Promise((solve,rej) => {
-        axios.get("https://www.tabroom.com/index/search.mhtml?search="+search)
+    return new Promise((solve, rej) => {
+        axios.get("https://www.tabroom.com/index/search.mhtml?search=" + search)
             .then((d) => {
                 res = [];
                 var query = cheerio.load(d.data)
                 query("tbody").each((_, table) => {
-                    query(table).children().map((_,row) => {
+                    query(table).children().map((_, row) => {
                         var tournament = {}
-                        query(row).children().map((idx,desc) => {
-                            if (idx != 0){
-                                var col = desc.children[0].data.trim().replace(/\n/g,'').replace(/\t/g,' ');
+                        query(row).children().map((idx, desc) => {
+                            if (idx != 0) {
+                                var col = desc.children[0].data.trim().replace(/\n/g, '').replace(/\t/g, ' ');
                                 switch (idx) {
                                     case 1:
                                         tournament.name = desc.children[1].children[0].data.trim()
@@ -47,15 +46,15 @@ const query = (search) => {
                 })
                 solve(res)
             })
-            })
+    })
 }
 var getTabs = id => {
-    return new Promise((solve,rej) => {
-        axios.get("https://www.tabroom.com/index/tourn/index.mhtml?tourn_id="+id)
+    return new Promise((solve, rej) => {
+        axios.get("https://www.tabroom.com/index/tourn/index.mhtml?tourn_id=" + id)
             .then(d => {
                 res = []
                 var query = cheerio.load(d.data)
-                query("#tabnav").children().map((_,li) => {
+                query("#tabnav").children().map((_, li) => {
                     res.push(li.children[1].children[0].data.trim())
                 })
                 solve(res)
@@ -64,20 +63,20 @@ var getTabs = id => {
 }
 var fetchEvents = id => {
     return new Promise(res => {
-        axios.get("https://www.tabroom.com/index/tourn/fields.mhtml?tourn_id="+id)
-        .then(d => {
-            var rounds = []
-            var query = cheerio.load(d.data)
-            query(".sidenote").children().map((_,v) => {
-                if (v.attribs.href) {
-                    rounds.push({eventName:v.children[0].data.trim(),eventID:v.attribs.href.split("/index/tourn/fields.mhtml?tourn_id="+id+"&event_id=")[1]})
-                }
+        axios.get("https://www.tabroom.com/index/tourn/fields.mhtml?tourn_id=" + id)
+            .then(d => {
+                var rounds = []
+                var query = cheerio.load(d.data)
+                query(".sidenote").children().map((_, v) => {
+                    if (v.attribs.href) {
+                        rounds.push({ eventName: v.children[0].data.trim(), eventID: v.attribs.href.split("/index/tourn/fields.mhtml?tourn_id=" + id + "&event_id=")[1] })
+                    }
+                })
+                res(rounds)
             })
-            res(rounds)
-        })
     })
 }
-var getPairings = (tournament,round) => {
+var getPairings = (tournament, round) => {
     return new Promise(solve => {
         axios.get(`https://www.tabroom.com/index/tourn/postings/round.mhtml?tourn_id=${tournament}&round_id=${round}`)
             .then((d) => {
@@ -90,14 +89,14 @@ var getPairings = (tournament,round) => {
                 });
                 query('tbody > tr').toArray().map((item, i) => {
                     res[i] = {}
-                    cheerio.load(item)('tr > td').toArray().map((item,_) => {
+                    cheerio.load(item)('tr > td').toArray().map((item, _) => {
                         cheerio.load(item)("a").toArray().map(x => {
                             id = x.attribs.href.trim()
                             id = url.parse(id, true).query
                             delete id["tourn_id"]
                             id = Object.values(id)[0]
                         })
-                        res[i][head[_]] = {name:query(item).text().trim().replace(/\n/g,'').replace(/\t/g,' '),id:id};
+                        res[i][head[_]] = { name: query(item).text().trim().replace(/\n/g, '').replace(/\t/g, ' '), id: id };
                     });
                 });
                 solve(res)
@@ -106,17 +105,37 @@ var getPairings = (tournament,round) => {
 }
 var getRounds = (tourn, event) => {
     return new Promise(solve => {
-        axios({url:"https://www.tabroom.com/index/tourn/postings/index.mhtml",method:"POST",data:qs.stringify({tourn_id:tourn,event_id:event})})
+        axios({ url: "https://www.tabroom.com/index/tourn/postings/index.mhtml", method: "POST", data: qs.stringify({ tourn_id: tourn, event_id: event }) })
             .then(d => {
                 var rounds = []
                 var query = cheerio.load(d.data)
-                query(".sidenote").children().map((_,v) => {
+                query(".sidenote").children().map((_, v) => {
                     if (v.attribs.href) {
-                        rounds.push({roundName:v.children[0].data.trim().replace(/\n/g,'').replace(/\t/g,' '),roundID:v.attribs.href.split("/index/tourn/postings/round.mhtml?tourn_id="+
-                        tourn+"&round_id=")[1]})
+                        rounds.push({
+                            roundName: v.children[0].data.trim().replace(/\n/g, '').replace(/\t/g, ' '), roundID: v.attribs.href.split("/index/tourn/postings/round.mhtml?tourn_id=" +
+                                tourn + "&round_id=")[1]
+                        })
                     }
                 })
                 solve(rounds)
             })
     })
+}
+var getParadigm = (tourn, judge) => {
+    return new Promise(solve => {
+        axios.get(`https://www.tabroom.com/index/tourn/postings/judge.mhtml?judge_id=${judge}&tourn_id=${tourn}`)
+            .then(d => {
+                var query = cheerio.load(d.data)
+                solve(query(".paradigm").html())
+            })
+    })
+}
+module.exports = {
+    getToken: getToken,
+    query: query,
+    getTabs: getTabs,
+    fetchEvents: fetchEvents,
+    getPairings: getPairings,
+    getRounds: getRounds,
+    getParadigm:getParadigm
 }
